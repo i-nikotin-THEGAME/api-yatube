7b4978b9e0fd99731ae73a17c287fb360e794668
i-nikotin-THEGAME <i_nikotin@thegame.design>
2026-06-04 11:56:05 +0300

    CRUD для Yatube

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -10,7 +10,10 @@ app_name = "api"
 router = routers.DefaultRouter()
 router.register(r"posts", PostViewSet, basename="posts")
 router.register(r"groups", GroupViewSet, basename="groups")
-router.register(r"posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comments")
+router.register(
+    r"posts/(?P<post_id>\d+)/comments",
+    CommentViewSet,
+    basename="comments")
 
 urlpatterns = [
     path('', include(router.urls)),
